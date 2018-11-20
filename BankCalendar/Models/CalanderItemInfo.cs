c4a9c922e82e1ebee26fd99c7ef7369bcf9a5d25
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-20 13:50:53 +1300

    .gitignore fix

---
+++ BankCalendar/Models/CalanderItemInfo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankCalendar.Models
+{
+    public class CalanderItemInfo
+    {
+        public string Date { get; set; }
+        public string Event { get; set; }
+        public IFormFile 
+    }
+}
